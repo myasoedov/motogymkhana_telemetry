800bf3c052fb9f12b04474439b8cf2d08f9966bb
Anton Kirsanov <lp2m10xdynt5sxe@gmail.com>
2024-05-01 19:01:31 +0400

    Optimized UI performance

--- arduino/telemetry_master.ino
+++ arduino/telemetry_master.ino
@@ -3,10 +3,10 @@
 constexpr uint8_t LCD_ADDR = 0x27;
 
 constexpr uint8_t START_BUTTON_PORT     = 4;
-constexpr uint8_t START_GATE_PORT       = 6;
-constexpr uint8_t FINISH_GATE_PORT      = 7;
-constexpr uint8_t SEMAPHORE_RED_PORT    = 9;
-constexpr uint8_t SEMAPHORE_YELLOW_PORT = 11;
+constexpr uint8_t START_GATE_PORT       = 7;
+constexpr uint8_t FINISH_GATE_PORT      = 6;
+constexpr uint8_t SEMAPHORE_RED_PORT    = 11;
+constexpr uint8_t SEMAPHORE_YELLOW_PORT = 9;
 constexpr uint8_t SEMAPHORE_GREEN_PORT  = 10;
 
 constexpr long SEMAPHORE_STOP_TIME      = 2000;
@@ -20,9 +20,9 @@ constexpr long FINISH_GATE_COOLDOWN = 5000;
 
 constexpr long LINE_MOVE_TIME = 500;
 constexpr long LINE_MOVE_STEP = 2;
-constexpr long UI_UPDATE_TIME = 100;
+constexpr long UI_UPDATE_TIME = 300;
 
-const char vStringBuffer[21];
+char vStringBuffer[21];
 const char vLineBuffer[21];
 const char vTimeStringBuffer[20];
 const char vReactionTimeStringBuffer[10];
@@ -627,14 +627,14 @@ public:
     return digitalRead(START_BUTTON_PORT) == HIGH;
   }
   bool isSomebodyInStartGate() {
-    return digitalRead(START_GATE_PORT) == HIGH;
+    return digitalRead(START_GATE_PORT) == LOW;
   }
 };
 
 class CFinishGateHandlerImpl : public CFinishGate::IHandler {
 public:
   bool isSomebodyInFinishGate() {
-    return digitalRead(FINISH_GATE_PORT) == HIGH;
+    return digitalRead(FINISH_GATE_PORT) == LOW;
   }
 };
 
@@ -705,6 +705,25 @@ constexpr const char* TEXT_GATE_OPEN = "Race started";
 constexpr const char* TEXT_EMPTY = "";
 const char* vTextState = "";
 
+char vSpaceLine[21] = "                    ";
+char vLcdLines[4][21] = {"", "", "", ""};
+int vTextLineWritten = 0;
+
+void printLine(unsigned int lineNum, const char * text) {
+  if (strcmp(vLcdLines[lineNum], text) == 0) return;
+
+  lcd.setCursor(0, lineNum);
+  vTextLineWritten = lcd.print(text);
+
+  if (strlen(vLcdLines[lineNum]) > strlen(text) && vTextLineWritten < 20) {
+    vSpaceLine[20 - vTextLineWritten] = 0;
+    lcd.print(vSpaceLine);
+    vSpaceLine[20 - vTextLineWritten] = " ";
+  }
+
+  strcpy(vLcdLines[lineNum], text);
+}
+
 void showRaceInfo() {
   switch(vState.getState()) {
     case CState::STATE_READY:
@@ -737,28 +756,28 @@ void showRaceInfo() {
     vTextState
   );
 
-  lcd.setCursor(0, 0);
-  lcd.print(_toLineBuffer(vStringBuffer));
-
+  printLine(0, vStringBuffer);
 }
 
 void showRaceTimes() {
   CRace* race = vState.getTrack()->lastRace();
   if (race && !race->isFinished()) {
     lcd.setCursor(0, 1);
-    lcd.print(_toLineBuffer(race->toString()));
+    printLine(1, race->toString());
+  } else {
+    printLine(1, "No one on track");
   }
   
   race = vState.getTrack()->lastFinishedRace();
   if (race) {
     lcd.setCursor(0, 2);
-    lcd.print(_toLineBuffer(race->toString()));
+    printLine(2, race->toString());
   }
 
   race = vState.getTrack()->finishedRaceLastWithOffset(1);
   if (race) {
     lcd.setCursor(0, 3);
-    lcd.print(_toLineBuffer(race->toString()));
+    printLine(3, race->toString());
   }
 }
 
